5636017836a51ee892b9114f322ad14da2f7f534
Juan Camilo Castellanos Puentes <camilo.castellanos.puentes@gmail.com>
2025-08-23 23:59:09 -0500

    Se crea la pagina Home para indicar que es un proyecto de pruebas y también se adiciona un navbar para redireccionamiento entre paginas

--- frontendSignalR/src/app/app.routes.ts
+++ frontendSignalR/src/app/app.routes.ts
@@ -1,8 +1,13 @@
 import { Routes } from '@angular/router';
 import { KitchenComponent } from './pages/food/kitchen/kitchen.component';
 import { CustomersComponent } from './pages/food/customers/customers.component';
+import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
+
+  { path: 'home', component: HomeComponent },
   { path: 'customers', component: CustomersComponent },
   { path: 'kitchen', component: KitchenComponent },
+  { path: '**', component: HomeComponent },
+  { path: '', component: HomeComponent },
 ];
